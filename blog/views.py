1bd8390f428b4ee69dd1871a84b962625caa6386
HuiseongK <rkdgmltjd00@naver.com>
2024-08-27 12:19:08 +0900

    FBV blog page 생성

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def index(request):
+
+    posts = Post.objects.all().order_by('-pk') # 최근글부터 보이도록 정렬
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts':posts,
+        }
+    )
